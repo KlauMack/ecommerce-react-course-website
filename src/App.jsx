519d07d68231e3777997465f221baff4f329cb9c
Klaudijus Mackonis <klaudijus.mackonis@gmail.com>
2026-04-03 14:39:39 +0300

    added registration/login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import './App.css'
+import "./App.css";
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import Auth from '../pages/Auth'
-import Checkout from '../pages/Checkout'
-import Home from '../pages/Home'
-import Navbar from '../components/Navbar'
+import Auth from "./pages/Auth";
+import AuthProvider from "./context/AuthContext";
+import Checkout from "./pages/Checkout";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/auth" element={<Auth />}/>
-        <Route path="/checkout" element={<Checkout />}/>
-      </Routes>
-    </div>
-  )
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
