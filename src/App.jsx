51584cbcfb8749e8ae9c315856b5c9fac4d2f5f8
Klaudijus Mackonis <klaudijus.mackonis@gmail.com>
2026-04-05 20:39:11 +0300

    finished cart and checkout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Auth from "./pages/Auth";
 import AuthProvider from "./context/AuthContext";
+import CartProvider from "./context/CartContext";
 import Checkout from "./pages/Checkout";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
@@ -12,15 +13,17 @@ import ProductDetails from "./pages/ProductDetails";
 function App() {
   return (
     <AuthProvider>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </CartProvider>
     </AuthProvider>
   );
 }
